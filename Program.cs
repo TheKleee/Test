296dd35d96f4a1063e5c20ea09804efb0ad160b6
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-08-01 15:45:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Testing async operations through the Task Main()
+ */
+
+using System;
+using System.Collections.Generic;
+using TestNS;
+
+#region Test:
+namespace TestNS
+{ 
+    interface iTest
+    {
+        public int[] ints { get; set; } 
+    }
+
+    public class Test: iTest
+    {
+        public int[] ints { get; set; }
+        public int a, b, c;
+
+        public Test(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+            ints = new int[] { a, b, c };
+        }
+    }
+
+    public class NewTest : Test
+    {
+        public int d;
+        public NewTest(int x, int y, int z, int d) : base(x, y, z)
+        {
+            this.d = d;
+
+            ints = new int[] { x, y, z, d };
+        }
+}
+}
+#endregion test />
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var t = new NewTest(1, 2, 3, 4).Sum();
+        var t2 = new Test(1, 2, 3).Sum();
+
+        Console.WriteLine($"{t} + {t2} = {t + t2}");
+    }
+}
+
+public static class TestSum
+{
+    public static int Sum(this Test t)
+    {
+        int s = 0;
+        for (int i = 0; i < t.ints.Length; i++)
+            s += t.ints[i];
+        return s;
+    }
+}
+
+
